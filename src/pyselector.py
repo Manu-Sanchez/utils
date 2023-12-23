473d7a132602306e113f5e799882d0de364691cd
msancmas <manuel.sanchezmascarell@emeal.nttdata.com>
2023-12-23 16:46:39 +0100

    Added filter allow/disallow, filter case sensitive option, and return value

--- src/pyselector.py
+++ src/pyselector.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import time
 import keyboard
 
 RESET = '\033[0m'
@@ -12,11 +14,14 @@ CYAN = '\033[96m'
 
 idx = 0
 filter_term = ""
+aux_choice_list = []
 
 def _show(question, choice_list, color, idx, filter_term, window_size = 10):
 
     os.system("cls")
-    print(f"Filtering By: {filter_term}")
+
+    if filter_term:
+        print(f"Filtering By: {filter_term}")
 
     if choice_list: 
         window_side_size = int(window_size/2)
@@ -44,30 +49,36 @@ def _show(question, choice_list, color, idx, filter_term, window_size = 10):
 
 
 
-def keyboard_interaction(event, question, choice_list, carrousel, color):
+def keyboard_interaction(event, question, choice_list, allow_filter, case_sensitive, carrousel, color):
     
-    global filter_term, idx 
+    global filter_term, idx , aux_choice_list
 
     aux_choice_list = choice_list.copy()
     
     #Update the filter
-    if event.name == "backspace":
-        filter_term = filter_term[:-1]
+    if allow_filter:
+        if event.name == "backspace":
+            filter_term = filter_term[:-1]
 
-    elif len(event.name) == 1:
-        filter_term += event.name
+        elif len(event.name) == 1:
+            filter_term += event.name
 
 
-    if filter_term:
-        filtered_choice_list = []
-        for choice in aux_choice_list:
-            if filter_term in choice:
-                filtered_choice_list.append(choice)
+        if filter_term:
+            filtered_choice_list = []
+            for choice in aux_choice_list:
+                if not case_sensitive:
+                    if filter_term.lower() in choice.lower():
+                        filtered_choice_list.append(choice)
 
-        aux_choice_list = filtered_choice_list
+                else:
+                    if filter_term in choice:
+                        filtered_choice_list.append(choice)
 
-        if idx > len(aux_choice_list) - 1:
-            idx = len(aux_choice_list) - 1
+            aux_choice_list = filtered_choice_list
+
+            if idx > len(aux_choice_list) - 1:
+                idx = len(aux_choice_list) - 1
 
     #Interaction
     if event.name == "flecha arriba":
@@ -86,20 +97,18 @@ def keyboard_interaction(event, question, choice_list, carrousel, color):
         else:
             idx += 1
 
+    if event.name != "enter":
+        _show(
+            question,
+            aux_choice_list,
+            color,
+            idx,
+            filter_term
+        )
 
-    _show(
-        question,
-        aux_choice_list,
-        color,
-        idx,
-        filter_term
-    )
+def show_choice_list(question, choice_list, allow_filter=False, case_sensitive=True, carrousel=False, color=None):
 
-
-
-def show_choice_list( question, choice_list, carrousel=0, color=None):
-
-    global idx, filter_term
+    global idx, filter_term, aux_choice_list
 
     idx = 0
     filter_term = ""
@@ -118,34 +127,49 @@ def show_choice_list( question, choice_list, carrousel=0, color=None):
             event,
             question,
             choice_list,
+            allow_filter,
+            case_sensitive,
             carrousel,
             color
         )
     )
     keyboard.wait("enter")
-    
-if __name__ == "__main__":
-    print(show_choice_list(
-        "Select an item",
-        [
-            "apple",
-            "cheese",
-            "orange",
-            "milk",
-            "water",
-            "cola",
-            "onion",
-            "grapes",
-            "lemon",
-            "pineapple",
-            "juice",
-            "cucumber",
-            "watermelon"
-        ],
-        color = RED,
-        carrousel=1
-    ))
+    keyboard.unhook_all()
 
+    return aux_choice_list[idx]
+    
 
 
     
+if __name__ == "__main__":
+    
+    while True:
+        res = show_choice_list(
+            "Select an item",
+            [
+                "apple",
+                "cheese",
+                "orange",
+                "milk",
+                "water",
+                "cola",
+                "onion",
+                "grapes",
+                "lemon",
+                "pineapple",
+                "juice",
+                "cucumber",
+                "watermelon"
+            ],
+            color = RED,
+            carrousel=True,
+            allow_filter=True,
+            case_sensitive=False
+        )
+
+        os.system("cls")
+        print(f"You've selected: {res}")
+
+        time.sleep(2)
+
+
